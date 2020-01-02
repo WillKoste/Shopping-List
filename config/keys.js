b23d5da3a22cb5f85636d888b221ceb67d0b09c5
Will Koste <willkoste@gmail.com>
2020-01-02 10:03:21 -0500

    MongoDB and Express Setup

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://willkoste:My2dogs95@cluster0-ojfbl.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}*******`));
