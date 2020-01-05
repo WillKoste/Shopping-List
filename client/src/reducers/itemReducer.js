15c825d68ea5807b690b27a790563ffc60d02725
Will Koste <willkoste@gmail.com>
2020-01-04 19:48:53 -0500

    Add Items

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,20 +1,18 @@
 import uuid from 'uuid';
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM} from '../actions/types';
+import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from '../actions/types';
 
 const initialState = {
-  items: [
-    {id: uuid(), name: ':)'},
-    {id: uuid(), name: 'Please Work'},
-    {id: uuid(), name: 'WHY DOES IT WORK NOW????'},
-    {id: uuid(), name: 'just a bug?'}
-  ]
+  items: [],
+  loading: false
 }
 
 export default function(state = initialState, action){
   switch(action.type){
     case GET_ITEMS:
       return {
-        ...state
+        ...state,
+        items: action.payload,
+        loading: false
       }
     case DELETE_ITEM:
       return {
@@ -25,6 +23,11 @@ export default function(state = initialState, action){
       return{
         ...state,
         items: [action.payload, ...state.items]
+      }
+    case ITEMS_LOADING:
+      return{
+        ...state,
+        loading: true
       }
     default:
       return state;
